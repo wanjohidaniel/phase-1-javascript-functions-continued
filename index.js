cc0476d8842a4ea7742663bea1174ea1bc7cd266
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-03-22 07:53:59 -0700

    Fuction lads 2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity= "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(purpose= "go to the office"){
+    return `This Monday, I will ${purpose}.`
+}
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
